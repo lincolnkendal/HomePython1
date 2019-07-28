e85102a02b86431dbf59cc9dd453a07d9b9bafda
Kendal Loseke <38168874+lincolnkendal@users.noreply.github.com>
2019-07-28 17:32:45 -0500

    Add files via upload

---
+++ WebPg.py
@@ -0,0 +1,14 @@
+class Page1:
+
+    age = 3
+
+    def __init__(self, someNumber):
+        self.age = someNumber
+        print ("passed in value is one less than " + str(someNumber + 1))
+
+
+    def fox(self):
+        print (self.age - 10)
+
+
+        return 3.4
