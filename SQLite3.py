9cbdcf69b5e1fac5055a1d1584fa345398103357
Kendal Loseke <38168874+lincolnkendal@users.noreply.github.com>
2019-08-22 21:01:07 -0500

    Adding
    Adding 2 py files

---
+++ SQLite3.py
@@ -0,0 +1,16 @@
+
+# from Gen_DB_classes.GenericDB_code import *
+
+#from SQLite3_classes.TestFrog1 import *
+
+from TestFrog1 import *
+# from Gen_DB_classes.GenericDB_code import *
+
+#class Sql3Conn(dbConnection):
+class Sql3Conn(Frogsa):  #worked !
+
+#    from GenericDB_
+    rags = 5
+
+    def __init__(self, inCount):
+        rags = inCount
---
+++ TestFrog1.py
@@ -0,0 +1,3 @@
+class Frogsa:
+    pass
+    
