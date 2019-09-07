c76401344010d8bfcc69419e435e36f2d7428d6b
Kendal Loseke <38168874+lincolnkendal@users.noreply.github.com>
2019-09-07 17:19:49 -0500

    Add files via upload
    Now able to read a table!

--- SQLite3_classes/SQLite3.py
+++ SQLite3_classes/SQLite3.py
@@ -1,16 +1,36 @@
 
-# from Gen_DB_classes.GenericDB_code import *
+# Changed 9/7/2019
 
-#from SQLite3_classes.TestFrog1 import *
-
-from TestFrog1 import *
-# from Gen_DB_classes.GenericDB_code import *
+import sqlite3
+import os
 
 #class Sql3Conn(dbConnection):
-class Sql3Conn(Frogsa):  #worked !
+class SQLite:
 
-#    from GenericDB_
-    rags = 5
+    #yug = dbConnection()
+    #yug.foo()
 
-    def __init__(self, inCount):
-        rags = inCount
+    # import sqlite3
+
+    path = 'init'
+    def __init__(self, in_Name: str):
+        self.name = in_Name
+        # groink = dbConnection("Tommy")
+        # groink.foo()
+
+    def DBconnect(self, inPath):
+        path = inPath
+
+        connection = sqlite3.connect(path)
+        #connection = connect(path)
+
+        path = os.path.abspath(path)
+        cur1 = connection.cursor()
+        cur1.execute("SELECT * FROM goof1")
+        rows = cur1.fetchall()
+
+        for row in rows:
+            print(row)
+
+        connection.close()
+        #print(is_open(path))
