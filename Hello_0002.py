6b2c94a08c26085464d12c7f2cd29970bd700982
Kendal Loseke <38168874+lincolnkendal@users.noreply.github.com>
2019-09-21 21:31:22 -0500

    Add files via upload
    Updated goals in comments at the top of the file.

--- Hello_0002.py
+++ Hello_0002.py
@@ -1,11 +1,21 @@
-#changed 9/7/2019
+#changed 9/21/2019
+
+# Learn this:
+# 1. Project container & Management in Atom
+#       Atom has no projects, only folders.
+# 2. Commiting Code changes into github.com from within Atom
+#       See video -->> https://www.youtube.com/watch?v=HZV7OKoD1Hc
+#       Push to Github from Atom
+#       Problem button says --> "Create detached commit"
+# 3. Master Python import tech well enough to use a separate class for the SQLite connection
+# 4. Debugging Python in Atom
 
 import datetime
 from WebPg import *
 import DB_Classes
 
-#from DB_Classes.dbConnection import dbConn
-from DB_Classes.SQLite3 import SQLite
+#from DB_Classes. import dbConn
+from DB_Classes.SQLite3 import KSQLite
 
 curTime = datetime.datetime.now()
 print ("Time is " + str(curTime))
@@ -15,5 +25,5 @@ x = Page1(38)
 print (x.fox())
 print (x.stringLength("seven"))
 
-tiger = SQLite(r'Thomas')
+tiger = KSQLite(r'Thomas')
 tiger.DBconnect(r'C:\Users\Loseke\Desktop\Code\SQLite_DBs\scrape.db')
